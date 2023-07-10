b2f96305a186b187a4c271c82111e76fe078bc77
Hlynur Isak <isakvillason@hotmail.com>
2023-07-10 14:53:14 +0000

    Added from pricelist project -Hlynur

---
+++ verdlisti_df.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+# from string import replace
+
+class verdlisti():
+    def __init__(self) -> None:
+        self.df = pd.read_csv('verdlisti.csv', sep=';')
+        
+        
+    def change_currency(self, currency) -> pd.DataFrame:
+        """Changes the currency of all prices in dataframe"""
+        exchange_rate = {'ISK': 140, 'USD': 1.0723, 'EUR': 0.94}
+        
+        if currency == 'USD':
+            self.df['Currency'], self.df['Price'] = currency, np.where(self.df['Currency'] == currency, self.df['Price'], np.round(pd.to_numeric(self.df['Price'].str.replace(',','.')) * exchange_rate[currency], 2))
+        elif currency == 'EUR':
+            self.df['Currency'], self.df['Price'] = currency, np.where(self.df['Currency'] == currency, self.df['Price'], np.round(pd.to_numeric(self.df['Price'].str.replace(',','.')) * exchange_rate[currency], 2))
+        return self.df
+    
+    def sort_by_price(self, amount:int) -> pd.DataFrame:
+        """Sorts values by price"""
+        self.df = self.df.sort_values(by='Price', ascending=False).head(int(amount))
+        return self.df
+    
+    def show_only(self, vendor):
+        """Shows only selected vendor"""
+        if vendor != 'All':
+            self.df = self.df[self.df['Vendor'] == vendor]
+        return self.df
+    
+    def __repr__(self) -> str:
+        return self.df.__repr__()
