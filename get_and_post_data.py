a334a0930b0b8cccb82af6fdf93532ab6df0949f
Hlynur Isak <isakvillason@hotmail.com>
2023-07-14 15:20:26 +0000

    Added return types

--- get_and_post_data.py
+++ get_and_post_data.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 class data_import:
-    def __init__(self, file_name):
+    def __init__(self, file_name) -> None:
         self.data_frame = self.import_csv(file_name)
 
-    def import_csv(self, file_name):
+    def import_csv(self, file_name) -> pd.DataFrame:
         df = pd.read_csv(file_name+'.csv')
         return df
