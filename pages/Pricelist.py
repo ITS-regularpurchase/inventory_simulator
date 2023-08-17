3580c6ad11b66d834437ef77fa834be9635de2a2
Hlynur Isak <isakvillason@hotmail.com>
2023-08-17 15:25:38 +0000

    Currency exchange working but with a bug

--- pages/Pricelist.py
+++ pages/Pricelist.py
@@ -78,7 +78,21 @@ if check_password():
             builder.configure_column("PNR", rowGroup=True)
             builder.configure_column("Price", aggFunc='min')
             pl_go = builder.build()
-            displayed_grid = AgGrid(pl.df.loc[(pl.df['PNR'].str.contains(search, case=False))],
+            this_grid = pl.df.loc[(pl.df['PNR'].str.contains(search, case=False))]
+            displayed_grid = AgGrid(this_grid,
+                                gridOptions=pl_go,
+                                height=500,
+                                sorted_col='PNR',
+                                reload_data=False,
+                                fit_columns_on_grid_load=True,
+                                columns_auto_size_mode=ColumnsAutoSizeMode.FIT_ALL_COLUMNS_TO_VIEW,
+                                update_on=['cellValueChanged']
+                                )
+            
+            if currency != 'Default':
+                st.write(f"#### *Verð í {currency}:*")
+                this_grid = pl.change_currency(currency).loc[(pl.df['PNR'].str.contains(search, case=False))]
+                displayed_grid = AgGrid(this_grid,
                                 gridOptions=pl_go,
                                 height=500,
                                 sorted_col='PNR',
