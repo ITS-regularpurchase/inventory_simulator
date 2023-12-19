0233a84afa3c89cb0c639cf4d6479d6a6cbcf859
Hlynur Isak <isakvillason@hotmail.com>
2023-12-19 15:52:59 +0000

    added page

---
+++ pages/Innkaupatillaga.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from authentication import check_password
+
+
+st.set_page_config(
+    page_title="History",
+    page_icon="✈️",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        # 'Get Help': "mailto:sigurduroli@icelandair.is?subject=st.help&body=Mig vantar aðstoð með ...",
+        # 'Report a bug': "mailto:sigurduroli@icelandair.is?subject=st.bug&body=Það er villa í ...",
+        'About': "### Inventory Simulator \n ## Tengiliður: *sigurduroli@icelandair.is* \n\n---"
+    }
+)
+
+if check_password():
+    st.title("Hér er gott að fara í gegnum innkaupatillögu")
